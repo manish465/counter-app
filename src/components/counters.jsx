00ebc5d1cd7c4df70d25a3bafecccef619d14fe9
manish465 <ms5978307@gmail.com>
2020-02-21 21:55:30 -0800

    commit-4

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -4,9 +4,9 @@ import Counter from "./counter";
 class Counters extends Component {
     state = {
         counters: [
-            { id: 1, value: 4 },
+            { id: 1, value: 0 },
             { id: 2, value: 0 },
-            { id: 3, value: 2 },
+            { id: 3, value: 0 },
             { id: 4, value: 0 }
         ]
     };
